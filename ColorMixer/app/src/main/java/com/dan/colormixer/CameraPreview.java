c684ff9e05a1aa44957b5997d72bd1a4c0d99da3
bobby <bobbydonald25@gmail.com>
2014-12-21 12:25:06 -0500

    material design

---
+++ ColorMixer/app/src/main/java/com/dan/colormixer/CameraPreview.java
@@ -0,0 +1,121 @@
+package com.dan.colormixer;
+
+import java.io.IOException;
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+/** A basic Camera preview class */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private Boolean paused = false;
+
+    @SuppressWarnings("deprecation")
+	public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+        mCamera.setDisplayOrientation(90);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+    	 
+    	
+    	if (holder == null){
+    		Log.d("surfaceCreated", "holder is null");
+    	}
+    	try {
+            if (mCamera != null) {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+                Toast resume = Toast.makeText(Color.mContext, "Touch Preview to Pause", 5);
+                resume.show();
+            }
+        } catch (IOException exception) {
+            Log.e("surfaceCreated in CameraPreview", "IOException caused by setPreviewDisplay() or startPreview()" + exception.getMessage());
+        }
+        
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    	mCamera.stopPreview();
+        mCamera.setPreviewCallback(null); 
+		mCamera.release();
+		mCamera = null;
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+    	
+    	
+        if (mHolder.getSurface() == null){
+          // preview surface does not exist
+          return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+          // ignore: tried to stop a non-existent preview
+        }
+//        Log.d("CameraPreview", "Camera is:" + mCamera.toString());
+//        Camera.Parameters parameters = mCamera.getParameters();
+//        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
+//        int height = sizeList.get(0).height;
+//        int width = sizeList.get(0).width;
+//        parameters.setPreviewSize(width, height);
+//        mCamera.setParameters(parameters);
+//        holder.setFixedSize(width, height);
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+       
+        
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("CameraPreview", "Error starting camera preview: " + e.getMessage());
+        }
+
+    }
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		if(paused == false) {
+			Toast pause = Toast.makeText(Color.mContext, "Touch Preview to Resume", 3);
+			pause.show();
+			paused = true;
+			mCamera.stopPreview();
+		} else {
+			paused = false;
+			mCamera.startPreview();
+		}
+		
+		return super.onTouchEvent(event);
+	}
+    
+    
+    
+}
---
+++ ColorMixer/app/src/main/java/com/dan/colormixer/SavedColors.java
@@ -0,0 +1,83 @@
+package com.dan.colormixer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SavedColors extends ActionBarActivity {
+	private Button ok;
+	private Button cancel;
+	private ListView favorites;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.favorites);
+		ok = (Button) findViewById(R.id.bOk);
+		favorites = (ListView) findViewById(R.id.lvFavorites);
+		
+		// This is the array adapter, it takes the context of the activity as a 
+        // first parameter, the type of list view as a second parameter and your 
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Color.favoriteColors );
+        favorites.setAdapter(arrayAdapter); 
+        registerForContextMenu(favorites);
+
+        favorites.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				
+				Color.screen.setBackgroundColor((int) Long.parseLong(Color.favoriteColors.get(position), 16));
+			}
+        	
+        });
+		
+		ok.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				finish();
+			}			
+		});
+		
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+	    ContextMenuInfo menuInfo) {
+		 super.onCreateContextMenu(menu, v, menuInfo);
+		 if (v.getId()==R.id.lvFavorites) {
+	          MenuInflater inflater = getMenuInflater();
+	          inflater.inflate(R.menu.favorites, menu);
+	      }
+	}
+	
+	/*Delete favorite color*/
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+	      AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	      switch(item.getItemId()) {
+	         case R.id.MENU_ITEM_DELETE: //was delete
+	        	 Color.favoriteColors.remove(info.position);
+	        	 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Color.favoriteColors );
+	             favorites.setAdapter(arrayAdapter); 
+	             registerForContextMenu(favorites);
+	             
+	             return true;
+	         
+	          default:
+	        	  return super.onContextItemSelected(item);
+	      }
+	}
+}
